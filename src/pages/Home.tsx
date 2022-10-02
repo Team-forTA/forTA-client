0288ef713e930a7146783c1f39aea1335ef76db6
Minyoung Lee <leemin02_@naver.com>
2022-10-02 12:07:46 +0900

    feat(Home): make buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>Home</div>
+      <Button variant="contained">Sign In</Button>
+      <Button variant="contained">Sign Up</Button>
+      <br />
+      <Button variant="contained">Go</Button>
+    </div>
+  );
 };
 
 export default Home;
