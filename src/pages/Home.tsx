b98130646c36c810f4ef43e5fc62a4cb1ab506cf
Minyoung Lee <leemin02_@naver.com>
2022-10-09 10:25:43 +0900

    feat(Home): apply rename component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { TextLink, Hspace, Wspace } from 'components/common';
 
 const Home: React.FC = () => {
   return (
     <div>
       <div>Home</div>
-      <Button variant="contained">Sign In</Button>
-      <Button variant="contained">Sign Up</Button>
-      <br />
-      <Button variant="contained">Go</Button>
+      <Button variant="contained">
+        <TextLink to="/" name="Sign In" />
+      </Button>
+      <Wspace width={1} />
+      <Button variant="contained">
+        <TextLink to="/" name="Sign Up" />
+      </Button>
+      <Hspace height={2} />
+      <Button variant="contained">
+        <TextLink to="/" name="GO" />
+      </Button>
     </div>
   );
 };
