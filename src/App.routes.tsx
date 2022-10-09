045a2a1fcceb712b8b3678ac52960b4977987166
Minyoung Lee <leemin02_@naver.com>
2022-10-09 10:41:58 +0900

    feat: add signIn, singUp routes

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,11 +1,13 @@
-import Home from 'pages/Home';
 import React from 'react';
+import { Home, SignIn, SignUp } from 'pages';
 import { Route, Routes } from 'react-router-dom';
 
 export const AppRoute: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signIn" element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { TextLink, Hspace, Wspace } from 'components/common';
+import { CenteredLayout } from 'components/layout';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <CenteredLayout>
       <div>Home</div>
       <Button variant="contained">
-        <TextLink to="/" name="Sign In" />
+        <TextLink to="/signIn" name="Sign In" />
       </Button>
       <Wspace width={1} />
       <Button variant="contained">
-        <TextLink to="/" name="Sign Up" />
+        <TextLink to="/signUp" name="Sign Up" />
       </Button>
       <Hspace height={2} />
       <Button variant="contained">
         <TextLink to="/" name="GO" />
       </Button>
-    </div>
+    </CenteredLayout>
   );
 };
 
