faaf0a2399b3bf260b2464b1053f4a06f55f2ae6
Minyoung Lee <leemin02_@naver.com>
2022-10-09 11:42:36 +0900

    feat(SignUp): publishing signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Box, Button, ButtonGroup, styled, TextField } from '@mui/material';
+import { CenteredLayout } from 'components/layout';
+import { Hspace } from 'components/common';
+
+const StyledFormWrapper = styled('div')`
+  display: flex;
+  justify-content: center;
+`;
 
 const SignUp: React.FC = () => {
-  return <div>signup</div>;
+  return (
+    <CenteredLayout>
+      <Hspace height={5} />
+      <StyledFormWrapper>
+        <Box
+          component="form"
+          sx={{
+            width: '250px',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <TextField required label="Name" variant="outlined" type="text" />
+          <Hspace height={1} />
+          <TextField required label="Email" variant="outlined" type="email" />
+          <Hspace height={1} />
+          <TextField
+            required
+            label="Password"
+            variant="outlined"
+            type="password"
+          />
+          <Hspace height={1} />
+          <TextField
+            required
+            label="Comfirm Password"
+            variant="outlined"
+            type="password"
+          />
+          <Hspace height={1} />
+          <ButtonGroup fullWidth>
+            <Button variant="outlined">Teacher</Button>
+            <Button variant="outlined">Student</Button>
+          </ButtonGroup>
+          <Hspace height={1} />
+          <Button variant="contained">Sign UP</Button>
+        </Box>
+      </StyledFormWrapper>
+    </CenteredLayout>
+  );
 };
 
 export default SignUp;
